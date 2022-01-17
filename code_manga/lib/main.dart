79f94ae3ad5fa2fae7407ed9d41e7335e4359aed
BrenneBoss <tchalisantos40@gmail.com>
2022-01-17 19:33:49 -0300

    dando início a login page

--- code_manga/lib/main.dart
+++ code_manga/lib/main.dart
@@ -1,9 +1,8 @@
-//Inicio de um sonho!
-import 'package:flutter/cupertino.dart';
+import 'package:code_manga/pages/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.grey),
       home: CodeManga(),
     );
@@ -28,6 +28,6 @@ class CodeManga extends StatefulWidget {
 class _CodeMangaState extends State<CodeManga> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return loginPage();
   }
 }
