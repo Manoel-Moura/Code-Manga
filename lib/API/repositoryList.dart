8a5a586744858fdba1194d83a8bc25dfe7b576c5
Alexandre Nery <calexandre16347@gmail.com>
2022-02-22 21:43:06 -0300

    Fazendo Api Funfar Direitinho

---
+++ lib/API/repositoryList.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: file_names, unused_local_variable
+
+// import 'package:code_manga/API/modelo.dart';
+// import 'package:code_manga/API/repository.dart';
+import 'package:dio/dio.dart';
+// import 'package:code_manga/API/mangaApiModel.dart';
+import 'package:code_manga/API/listaMangaApiModel.dart';
+// import 'package:path/path.dart';
+// import 'package:provider/provider.dart';
+
+const String baseUrl = 'https://api.jikan.moe';
+const String endpoint = '/v4/manga';
+
+// Future<ListaMangaApiModel> buscaUmMangaNome(String name) async {
+//   Dio dio = Dio();
+//   dio.options.baseUrl = baseUrl;
+//   var resposta = await dio.get(endpoint + '?q=$name');
+
+//   if (resposta.statusCode == 200) {
+//     return ListaMangaApiModel.fromJson(resposta.data);
+//   }
+//   return ListaMangaApiModel();
+// }
+
+Future<ListaMangaApiModel> buscaListaDeMangas() async {
+  Dio dio = Dio();
+  dio.options.baseUrl = baseUrl;
+  var resposta = await dio.get(endpoint);
+
+  if (resposta.statusCode == 200) {
+    return ListaMangaApiModel.fromJson(resposta.data);
+  }
+  return ListaMangaApiModel();
+}
+
+Future<List<Data>?> recuperaMangas() async {
+  List<Data>? listaDeMangas = [];
+
+  ListaMangaApiModel lista = await buscaListaDeMangas();
+
+  listaDeMangas = lista.data!.getRange(0, 4).toList();
+
+  return listaDeMangas;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
+import 'dart:convert';
+// import 'dart:html';
+
+import 'package:code_manga/API/repositoryList.dart';
+import 'package:code_manga/API/repositoryUnity.dart';
 import 'package:code_manga/screens/navHomePage.dart';
 import 'package:flutter/material.dart';
-import 'package:code_manga/API/repository.dart';
+// import 'package:code_manga/API/repository.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
-void main() async {
+Future main() async {
   //print('O link de q preciso eh ${await getImageManga(2)}');
+  // final json = await fetch();
+  // print(json['url']);
   runApp(const MyApp());
 }
+
+// Future<Map> fetch() async {
+//   const String baseUrl = ('https://api.jikan.moe/v4/manga/10');
+//   var response = await http.get(Uri.parse(baseUrl));
+//   var json = jsonDecode(response.body);
+//   return json;
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Repository repo = Repository();
+    RepositoryUnity repo = RepositoryUnity();
     return Provider(
       create: (context) => repo,
       child: MaterialApp(
