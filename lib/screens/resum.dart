414c74f327053f90ca2ccb72982192553de758b0
BrenneBoss <tchalisantos40@gmail.com>
2022-02-26 00:20:24 -0300

    pagina de detalhes finalizada

--- lib/screens/resum.dart
+++ lib/screens/resum.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:code_manga/consts/colors/colors.dart';
 import 'package:code_manga/screens/navHomePage.dart';
 import 'package:code_manga/widgets/cabecalho.dart';
@@ -44,47 +47,181 @@ class Resume extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: (MediaQuery.of(context).size.height) / 1.5,
-                child: Image.network(
-                  url,
-                  fit: BoxFit.fitWidth,
-                  width: double.infinity,
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 40,
+                  bottom: 18,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 173,
+                      height: 275,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'https://cdn.myanimelist.net//images//manga//3//117681.jpg'),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(
+                                bottom: 16,
+                              ),
+                              child: Text(
+                                "Naruto Shippuden",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(
+                            bottom: 11,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Capítulos: ",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white54,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  left: 10,
+                                ),
+                                child: const Text(
+                                  "12",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(
+                            bottom: 11,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Volumes: ",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white54,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  left: 17,
+                                ),
+                                child: Text(
+                                  "10",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(
+                            bottom: 11,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Ranking: ",
+                                style: GoogleFonts.poppins(
+                                  textStyle: const TextStyle(
+                                    color: Colors.white54,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  left: 23,
+                                ),
+                                child: Text(
+                                  "10",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 35),
-                child: Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                        color: kprimaryColor,
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Resumo',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 19,
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 5, right: 5),
-                child: Text(
-                  'Resumo\n',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(color: kText, fontSize: 24),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(
+                      18,
+                    ),
+                    child: Text(
+                      'This is a good anime yeah i don´t know what to type here because i´m really sad cuz i want to sleep but i can´t cuz a wanna to finish this shit.This is a good anime yeah i don´t know what to type here because i´m really sad cuz i want to sleep but i can´t cuz a wanna to finish this shit.This is a good anime yeah i don´t know what to type here because i´m really sad cuz i want to sleep but i can´t cuz a wanna to finish this shit.',
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                          color: Colors.white60,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                child: Text(
-                  resume,
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(color: kText, fontSize: 18),
-                  ),
-                  textAlign: TextAlign.justify,
-                ),
-              ),
+                ],
+              )
             ],
           ),
         ),
